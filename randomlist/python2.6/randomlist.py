badbd2549fb7613131fdcf54d0fe05d5a5cee2e5
Paul Curry <pcurry@gmail.com>
2011-06-03 12:13:20 -0400

    Working implementation.
    Properly generates a random list and passes testing.  Does it using append on the Python list primative, which may be expensive.
    There may be a performance penalty.

--- randomlist/python2.6/randomlist.py
+++ randomlist/python2.6/randomlist.py
@@ -6,9 +6,15 @@ def random_list(ordered_list):
     if elements == 1:
         return [ordered_list[0]]
     random_list = []
-    for index in xrange(elements):
-        working_length = elements - index 
-        pick = randrange(working_length)
+    # for index in xrange(elements):
+    #     working_length = elements - index 
+    #     pick = randrange(working_length)
+    #     random_list.append(ordered_list[pick])
+    #     ordered_list[pick] = ordered_list[working_length - 1]
+    
+    while elements > 0:
+        pick = randrange(elements)
         random_list.append(ordered_list[pick])
-        ordered_list[pick] = ordered_list[working_length - 1]
+        elements = elements - 1
+        ordered_list[pick] = ordered_list[elements]
     return random_list
