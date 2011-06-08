7b0772abf96628a3693d97a103fe000ecee6967b
Paul Curry <pcurry@gmail.com>
2011-06-08 18:20:15 -0400

    Better implementations.
    While poking at this, I came to the conclusion that there were several ways to go about making roughly this algorithm work, at least in Python.  I wondered if creating the whole random list in the first place was less expensive than doing the creation by appending values, so I went with two different implementations of the randomization.  Then I figured out how to do it in a list comprehension, which I broke into 2 list comprehensions for readability.
    
    I covered the code with basic testing, but I'm still having problems with the list comprehension implementation misplacing elements.  I am working on that. Nevertheless, I want to get the code state up to github, so commiting now and sending.

--- randomlist/python2.6/randomlist_test.py
+++ randomlist/python2.6/randomlist_test.py
@@ -20,6 +20,82 @@ class TestRandomList(unittest.TestCase):
         self.assertEquals(2, len(output), "Number of elements changed.")
         self.assertEquals(set(source), set(output),
                           "Elements changed.")
+        self.assertNotEquals(output[0], output[1])
+        order_same = output == source
+        order_reversed = output == [second, first]
+        self.assert_(order_same or order_reversed,
+                     "The order is weird.")
+
+
+class TestRandomizeListInPlace(unittest.TestCase):
+
+    def test_two_element_list(self):
+        first = "first"
+        second = "second"
+        source = [first, second]
+        check = list(source)
+        randomlist.randomize_list_in_place(source)
+        self.assertEquals(2, len(source), "Number of elements changed.")
+        self.assertEquals(set(check), set(source),
+                          "Elements changed.")
+        self.assertNotEquals(source[0], source[1])
+        order_same = check == source
+        order_reversed = source == [second, first]
+        self.assert_(order_same or order_reversed,
+                     "The order is weird.")
+        
+
+
+# FIXME: There is a cleverer way to test all the randomization methods
+# FIXME: against the same set of tests. I don't remember how exactly
+# FIXME: at the moment, and I want these tests up now, but I will fix 
+# FIXME: this later.
+
+class TestRandomizeListAppend(unittest.TestCase):
+
+    def test_two_element_list(self):
+        first = "first"
+        second = "second"
+        source = [first, second]
+        output = randomlist.randomize_list_append(source)
+        self.assertEquals(2, len(output), "Number of elements changed.")
+        self.assertEquals(set(source), set(output),
+                          "Elements changed.")
+        self.assertNotEquals(output[0], output[1])
+        order_same = output == source
+        order_reversed = output == [second, first]
+        self.assert_(order_same or order_reversed,
+                     "The order is weird.")
+
+
+class TestRandomizeListComprehension(unittest.TestCase):
+
+    def test_two_element_list(self):
+        first = "first"
+        second = "second"
+        source = [first, second]
+        output = randomlist.randomize_list_comprehension(source)
+        self.assertEquals(2, len(output), "Number of elements changed.")
+        self.assertEquals(set(source), set(output),
+                          "Elements changed.")
+        self.assertNotEquals(output[0], output[1])
+        order_same = output == source
+        order_reversed = output == [second, first]
+        self.assert_(order_same or order_reversed,
+                     "The order is weird.")
+
+
+class TestRandomizeListInsert(unittest.TestCase):
+
+    def test_two_element_list(self):
+        first = "first"
+        second = "second"
+        source = [first, second]
+        output = randomlist.randomize_list_insert(source)
+        self.assertEquals(2, len(output), "Number of elements changed.")
+        self.assertEquals(set(source), set(output),
+                          "Elements changed.")
+        self.assertNotEquals(output[0], output[1])
         order_same = output == source
         order_reversed = output == [second, first]
         self.assert_(order_same or order_reversed,
