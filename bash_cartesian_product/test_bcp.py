5066e4c439bc752a890e641394b51d7951cb0871
Paul Curry <pcurry@gmail.com>
2016-12-12 03:50:46 -0500

    Test driven development is nice

--- bash_cartesian_product/test_bcp.py
+++ bash_cartesian_product/test_bcp.py
@@ -18,11 +18,26 @@ abijk abijl acdgijk acdgijl acegijk acegijl acfgijk acfgijl ahijk ahijl
 class SimpleTests(unittest.TestCase):
     
     def test_empty_string(self):
-        result = bcp.product("")
-        self.assertEquals("", result)
+        result = bcp.process_single_string("")
+        self.assertEqual("", result)
 
     def test_plain_string(self):
+        raw = "asdf"
+        result = bcp.process_single_string(raw)
+        self.assertEqual(raw, result)
+
+    def test_complicated_string(self):
+        raw = "a{b,c} d{e,f}"
+        expected = "ab ac de df"
+        result = bcp.process_string(raw)
+        self.assertEqual(expected, result)
         
+    def test_process_multiple_args(self):
+        raw = ['f{e,i}e', 'f{o,um}']
+        expected = "fee fie fo fum"
+        result = bcp.process_args(raw)
+        self.assertEqual(expected, result)
+
 
 if __name__ == "__main__":
     unittest.main()
