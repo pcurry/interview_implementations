0dd6b83ee1babbbeb68a12f6b5efb4bafcddecf3
Paul Curry <pcurry@gmail.com>
2020-06-20 12:59:47 -0400

    Test the other implementations also.

--- test_array_partitions.py
+++ test_array_partitions.py
@@ -3,35 +3,50 @@
 
 import unittest
 
-from array_partitions import equal_sum
+import array_partitions
 
 
 class TestEqualSum(unittest.TestCase):
 
+    def setUp(self):
+        self.equal_sum = array_partitions.equal_sum
 
     def test_empty_list(self):
-        self.assertIsNone(equal_sum([]))
+        self.assertIsNone(self.equal_sum([]))
 
     def test_single_element_list(self):
-        self.assertIsNone(equal_sum([3]))
+        self.assertIsNone(self.equal_sum([3]))
 
     def test_balanced_pairs(self):
         case_1 = [1, 1]
-        result_1 = equal_sum(case_1)
+        result_1 = self.equal_sum(case_1)
         self.assertEqual(result_1, ([1], [1]))
         case_2 = [1, 2, 1, 2]
-        result_2 = equal_sum(case_2)
+        result_2 = self.equal_sum(case_2)
         self.assertEqual(result_2, ([1, 2], [1, 2]))
 
     def test_descending_list(self):
         case = [3, 2, 1]
-        result = equal_sum(case)
+        result = self.equal_sum(case)
         self.assertEqual(result, ([3], [2, 1]))
 
     def test_ascending_list(self):
         case = [1, 1, 2]
-        result = equal_sum(case)
+        result = self.equal_sum(case)
         self.assertEqual(result, ([1, 1], [2]))
+
+
+
+class TestRecursiveEqualSum(TestEqualSum):
+
+    def setUp(self):
+        self.equal_sum = array_partitions.equal_sum_recursive
+
+
+class TestTailRecursiveEqualSum(TestEqualSum):
+
+    def setUp(self):
+        self.equal_sum = array_partitions.equal_sum_tail_recursive
 
 
 if __name__ == '__main__':
