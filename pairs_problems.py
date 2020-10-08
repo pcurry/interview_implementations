b99151259d72848d0034e71452cc663a5fc3bdde
Paul Curry <pcurry@gmail.com>
2020-10-08 19:21:36 -0400

    Implemented, but not tested yet.

--- pairs_problems.py
+++ pairs_problems.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 
 """
-Functions to count various features of ordered lists of integers
+Functions to count various features of lists of integers
 """
 
 
 import itertools
 
 from collections import Counter
-
+from collections import defaultdict
 
 def arbitrary_difference(numbers, difference):
     """
     Given a list of numbers and a value of a difference between a pair of
-    numbers chosen from the list, return the set of distinct pairs.
+    numbers chosen from the list, return the nuumber distinct pairs that satisfy
+    this difference.
     """
-
+    count = Counter(numbers)
+    pairs = set((a, a + difference) for a in count.keys() if count[a + difference] > 0)
+    return len(pairs)
 
 
 def equal_adjacent_pairs(numbers):
@@ -30,3 +33,22 @@ def equal_adjacent_pairs(numbers):
         if current == numbers[i + 1]:
             count[(current, current)] += 1
     return count
+
+
+def title_pairs_for_runtime(playtimes, runtime):
+    mod_times = defaultdict(list)
+    for i in range(len(playtimes)):
+        mod_times[playtimes[i] % runtime].append(i)
+    title_pairs = set()
+    for mt in mod_times.keys():
+        if mod_times[runtime - mt]:
+            for x in mod_times[mt]:
+                for y in mod_times[runtime - mt]:
+                    title_pairs.add((x, y))
+    if len(mod_times[0]) > 1:
+        even_length = mod_times[0]
+        for a in even_length:
+            for b in even_length:
+                if a != b:
+                    title_pairs.add((a, b))
+    return len(title_pairs)
